bef9beab35677ddf4e316968b8f0188cfa21fdf4
huiyaoren <wslshanlin@sina.com>
2016-11-03 14:38:59 +0800

    4.遍历数组

--- PHP_cookbook/session_04.php
+++ PHP_cookbook/session_04.php
@@ -39,3 +39,29 @@ function init_array_range(){
 	$odd = range(1, 52, 2);
 	$even = range(2, 52, 2);
 }
+
+
+// ----------------------------------------------------------------------------
+// 4.遍历数组
+function go_through_array(){
+	$array = [];
+
+	foreach($array as $value){
+	}
+
+	foreach($array as $key => $value){
+	}
+
+	for($key=0; $size=count($array);  $key++){
+	}
+
+	reset($array);
+	while (list($key, $value) = each($array)){
+	}
+}
+// 迭代数组元素时用 foreach 语句更整洁
+// 使用 foreach 语句时 PHP 迭代的是相应数组的一个副本 不是数组本身
+// 使用 for语句 和 each()函数时 PHP 迭代的是原始的数组
+// 如果在循环内部修改数组 不应该使用 foreach
+// each() 返回的变量不是数组中的原始值 而是一个副本
+// 使用 each() 时 指针越过数组末端将返回 false
