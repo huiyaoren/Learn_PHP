dc959f0d6244d305d93e6107e1a05883dd116796
huiyaoren <wslshanlin@sina.com>
2016-11-04 13:24:00 +0800

    2.为变量设定默认值

--- PHP_cookbook/session_05.php
+++ PHP_cookbook/session_05.php
@@ -13,3 +13,13 @@ function equal_beset(){
 	// 但如果 0 == $string 时 PHP 会把右侧变量先转换为整型数 
 	// 为了避免此类问题 可以使用 0 === $string
 }
+
+
+// ----------------------------------------------------------------------------
+// 2.为变量设定默认值
+function var_default(){
+	$cars = isset($_REQUEST['cars']) ? $_REQUEST['cars'] : $default_cars;
+
+	// 对 0 false 值进行限制
+	$cars = $_REQUEST['cars'] ? $_REQUEST['cars'] : $default_cars;
+}
