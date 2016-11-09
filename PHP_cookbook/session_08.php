7eb26069fb08451dc5a8fb76dfd44905780476fe
huiyaoren <wslshanlin@sina.com>
2016-11-09 22:13:34 +0800

    2.读取 Cookie 的值

--- PHP_cookbook/session_08.php
+++ PHP_cookbook/session_08.php
@@ -20,5 +20,19 @@ function point_1(){
 
 	// 最后一个可选参数是一个安全标记 true 以 SSL 连接发送 cookie
 	// 但别忘了 cookie 还是以明文保存在客户端上
+}
 
+
+// ----------------------------------------------------------------------------
+// 2.读取 Cookie 的值
+function point_2(){
+	// 读取 cookie
+	if(isset($_COOKIE['flavor'])){
+		print "You ate a {$_COOKIE['flavor']} cookie.";
+	}
+
+	// 读取所有 cookie
+	foreach ($_COOKIE as $cookie_name => $cookie_value) {
+		print "$cookie_name = $cookie_value <br/>";
+	}
 }
