2e9876ac8b9989d846732dda7b05c164003b978b
huiyaoren <wslshanlin@sina.com>
2016-11-24 21:25:42 +0800

    5.通过任意方法定位 URL

--- PHP_cookbook/session_13.php
+++ PHP_cookbook/session_13.php
@@ -85,3 +85,57 @@ function point_4(){
 	$r->sendRequest();
 	$page = $r->getResponseBody();
 }
+
+
+// ----------------------------------------------------------------------------
+// 5.通过任意方法定位 URL
+function point_5(){
+	// 通过 http 流使用 put 方法
+	$url = 'http://www.example.com/put.php';
+	$body = '<menu>
+		<dish type="appetizer">Chicken Soup</dish>
+		<dish type="main course">Fried Monkey Brains</dish>
+	</menu>'; 
+	$options = array('method'=>'PUT', 'content'=>$body);
+	// 创建流环境
+	$context = stream_context_ctrate(array('http'=>$options));
+	// 把环境传递给 file_get_contents($url, false, $context);
+	print file_get_contents($url, false, $context);
+
+	// 通过 cURL 来使用 put 方法
+	$body = '<menu>
+	<dish type="appetizer">Chicken Soup</dish>
+	<dish type="main course">Fried Monkey Brains</dish>
+	</menu>';
+	$c = curl_init($url);
+	curl_setopt($c, CURLOPT_CUSTOMREQUEST, 'PUT');
+	curl_setopt($c, CURLOPT_POSTFIELDS, $body);
+	curl_setopt($c, CURLOPT_RETURNTRANSFER, true);
+	$page = curl_exec($c);
+	curl_close($c);
+
+	// 通过 HTTP_Request 来使用 put 方法 
+	require 'HTTP/Request.php';
+	$url = 'http://www.example.com/put.php';
+	$body = '<menu>
+	<dish type="appetizer">Chicken Soup</dish>
+	<dish type="main course">Fried Monkey Brains</dish>
+	</menu>';
+	$r = new HTTP_Request($url);
+	$r->setMethod(HTTP_REQUEST_METHOD_PUT);
+	$r->setBody($body);
+	$page = $r->getRespanseBody();
+
+	// 通过 cURL 和 put 来上传文件
+	$url = 'http://www.example.com/upload.php';
+	$filename = '/usr/local/data/pitures/piggy.jpg';
+	$fp = fopen($filename, 'r');
+	$c = curl_init($url);
+	curl_setopt($c, CURLOPT_PUT, ture);
+	curl_setopt($c, CURLOPT_INFILE, $fp);
+	curl_setopt($c, CURLOPT_INFILESIZE, filesize($filename));
+	curl_setopt($c, CURLOPT_RETURNTRANSFER, true);
+	$page = curl_exec($c);
+	print $page;
+	curl_close($c);
+}
